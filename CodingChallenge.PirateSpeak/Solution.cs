b47cf567a7811d745003b7c47722d3f26779a880
Ben Harrington <harringtonben@icloud.com>
2018-04-21 12:25:54 -0500

    finished first set of tests

--- CodingChallenge.PirateSpeak/Solution.cs
+++ CodingChallenge.PirateSpeak/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CodingChallenge.PirateSpeak
@@ -7,7 +8,37 @@ namespace CodingChallenge.PirateSpeak
     {
         public string[] GetPossibleWords(string jumble, string[] dictionary)
         {
-            throw new NotImplementedException();
+            List<string> matchList = new List<string>();
+
+            foreach (var word in dictionary)
+            {
+                if (jumble.Count() == word.Count())
+                {
+                    var compareStrings = CompareStrings(word, jumble);
+                    if (compareStrings)
+                        matchList.Add(word);
+                }
+            }
+
+            return matchList.ToArray();
+        }
+
+        public bool CompareStrings(string word, string jumble)
+        {
+            var jumbleAplha = jumble.OrderBy(x => x);
+            var wordAlpha = word.OrderBy(x => x);
+
+            return jumbleAplha.SequenceEqual(wordAlpha);
+        }
+
+    }
+
+    public static class Program
+    {
+        public static void  Main(string[] args)
+        {
+            var actualResult = new Solution().GetPossibleWords("ainstuomn", new[] { "mountains", "hills", "mesa" });
+
         }
     }
 }
